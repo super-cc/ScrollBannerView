9a220b6e34330888e746c7c98e1816f0c86e2cd7
guoshichao <guoshichao@blued.com>
2018-05-30 16:27:43 +0800

    [描述]添加两种方法 [模块]所有

--- app/src/main/java/com/cc/scrollbannerview/BannerViewActivity.java
+++ app/src/main/java/com/cc/scrollbannerview/BannerViewActivity.java
@@ -1,18 +1,22 @@
 package com.cc.scrollbannerview;
 
+import android.content.Intent;
 import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class BannerViewActivity extends AppCompatActivity {
 
     private NestedScrollView mScroll;
     private ScrollBannerView mBanner;
+    private TextView mTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_banner_view);
         initView();
     }
 
@@ -28,6 +32,14 @@ public class MainActivity extends AppCompatActivity {
         });
         mBanner = (ScrollBannerView) findViewById(R.id.banner);
         mBanner.setBannerImageResource(R.drawable.banner);
+
+        mTv = findViewById(R.id.tv);
+        mTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(BannerViewActivity.this, BannerAlphaActivity.class));
+            }
+        });
     }
 
 }
